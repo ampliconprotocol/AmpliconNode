f999ca692261a20bb7bd7d723564cd65195e375e
ChatOnBlockchain <108635067+ChatOnBlockchain@users.noreply.github.com>
2022-11-30 00:21:01 -0800

    Cleaned up unused sources

--- dna_search_trials_lookup_table.py
+++
@@ -1,6 +0,0 @@
-class DnaSearchTrialsLookupTable(object):
-    def __init__(self):
-        pass
-
-    def get_num_trials_for_finding_working_dna(self, dna_length: int) -> int:
-        return 10000
--- tests/test_dna_search_trials_lookup_table.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-from dna_search_trials_lookup_table import DnaSearchTrialsLookupTable
-
-class TestDnaSearchTrialsLookupTable(TestCase):
-    def test_get_num_trials_for_finding_working_dna(self):
-        dna_search_trials_lookup_table = DnaSearchTrialsLookupTable()
-        self.assertEqual(dna_search_trials_lookup_table.get_num_trials_for_finding_working_dna(128), 10000)
-
